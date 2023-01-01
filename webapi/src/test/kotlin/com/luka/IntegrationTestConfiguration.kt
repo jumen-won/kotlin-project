5e1b322bf34d7d54c460dd9be319b70e4ff83b4a
jumen-won <knoc5215@naver.com>
2023-01-01 22:59:43 +0900

    db명 변경

--- webapi/src/test/kotlin/com/luka/IntegrationTestConfiguration.kt
+++ webapi/src/test/kotlin/com/luka/IntegrationTestConfiguration.kt
@@ -2,7 +2,6 @@ package com.luka
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Testcontainers
 
@@ -10,12 +9,10 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @AutoConfigureMockMvc
 @Testcontainers
 abstract class IntegrationTestConfiguration {
-    // 해당 static 변수를 설정해야 컨테이너를 띄운다.
-    // database 이름은 하기의 이름과 yml 설정에서 같게 만든다.
     companion object {
         @JvmStatic
-        protected val dbContainer = PostgreSQLContainer<Nothing>("postgres:latest").apply {
-            withDatabaseName("test-database")
+        protected val dbContainer = PostgreSQLContainer<Nothing>("postgres:15").apply {
+            withDatabaseName("kotlin-project")
         }
 
         init {
